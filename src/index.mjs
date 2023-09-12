9f768c58cf5f3ae5afa5150f67bba54e418e96e2
ProXBucky <hungnguyenbn002@gmail.com>
2023-09-12 13:51:32 +0700

    add file db and React port

--- src/index.mjs
+++ src/index.mjs
@@ -26,7 +26,20 @@ app.get('*', (req, res) => {
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb', parameterLimit: 100000, }));
 app.use(cookieParser());
-app.use(cors())
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_PORT);
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 
 // Cấu hình kết nối Cloudinary
 cloudinary.config({
